152bcc96d827af1eb962f732a55c11f15a3d36e0
Timo Ebel <timo.ebel@gmail.com>
2020-03-01 02:00:47 +0100

    Another idea.

--- build-tools/build-push-docker.sh
+++ build-tools/build-push-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker login --username _json_key --password "$(echo $GCR_CREDENTIALS | base64 --decode -i)" https://eu.gcr.io
+docker login --username _json_key --password "$(echo ${GCR_CREDENTIALS} | base64 --decode -i)" https://eu.gcr.io
 
 # amd64
 buildctl build --frontend dockerfile.v0 \
