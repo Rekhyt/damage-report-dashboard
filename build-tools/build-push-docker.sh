4ced613fc6802c19d1c28c8715876e354b7fdf28
Timo Ebel <timo.ebel@gmail.com>
2020-03-01 23:56:37 +0100

    Typo

--- build-tools/build-push-docker.sh
+++ build-tools/build-push-docker.sh
@@ -20,9 +20,9 @@ buildctl build --frontend dockerfile.v0 \
             --local context=. \
             --frontend-opt platform=linux/armhf \
             --frontend-opt filename=./Dockerfile \
-            --output type=docker,name=eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}-arm7 \
+            --output type=docker,name=eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}-armv7 \
             --opt build-arg:IMAGE=balenalib/rpi-alpine-node:latest \
-            --opt build-arg:BUILD_PRODUCTION=true \
+            --opt build-arg:BUILD_PRODUCTION=false \
             --opt build-arg:API_URL=${API_URL} \
             | docker load
 
@@ -30,6 +30,7 @@ docker images
 
 # re-tag without arch for manifest
 docker tag eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}-amd64 eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}
+docker images
 
 # push to GCR
 docker login --username _json_key --password "$(echo ${GC_CREDENTIALS} | base64 --decode -i)" https://eu.gcr.io
