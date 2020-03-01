65c18c1801bf33e1d75c4b01c48a83eac6277704
Timo Ebel <timo.ebel@gmail.com>
2020-03-01 19:38:26 +0100

    Try pushing after build.

--- build-tools/build-push-docker.sh
+++ build-tools/build-push-docker.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-docker login --username _json_key --password "$(echo ${GC_CREDENTIALS} | base64 --decode -i)" https://eu.gcr.io
-
 # amd64
 buildctl build --frontend dockerfile.v0 \
             --local dockerfile=. \
             --local context=. \
             --exporter image \
             --exporter-opt name=eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}-amd64 \
-            --exporter-opt push=true \
+            --exporter-opt push=false \
             --frontend-opt platform=linux/amd64 \
             --frontend-opt filename=./Dockerfile \
             --opt build-arg:BUILD_PRODUCTION=true \
-            --opt build-arg:API_URL=http://192.168.2.150
+            --opt build-arg:API_URL=${API_URL}
 
 
 # arm
@@ -21,13 +19,17 @@ buildctl build --frontend dockerfile.v0 \
             --local context=. \
             --exporter image \
             --exporter-opt name=eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}-armv7 \
-            --exporter-opt push=true \
+            --exporter-opt push=false \
             --frontend-opt platform=linux/armhf \
             --frontend-opt filename=./Dockerfile \
             --opt build-arg:IMAGE=balenalib/rpi-alpine-node:latest \
             --opt build-arg:BUILD_PRODUCTION=true \
-            --opt build-arg:API_URL=http://192.168.2.150
+            --opt build-arg:API_URL=${API_URL}
 
+
+docker login --username _json_key --password "$(echo ${GC_CREDENTIALS} | base64 --decode -i)" https://eu.gcr.io
+docker push eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}-amd64
+docker push eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}-armv7
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
