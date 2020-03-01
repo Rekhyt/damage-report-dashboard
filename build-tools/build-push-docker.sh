de69eb10e4f2991b68c696ee62cc010d48a5c2e6
Timo Ebel <timo.ebel@gmail.com>
2020-03-01 01:37:35 +0100

    Build automation, push to GCR.

---
+++ build-tools/build-push-docker.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+echo $GCR_CREDENTIALS | base64 --decode -i | docker login --username _json_key --password-stdin https://eu.gcr.io
+
+# amd64
+buildctl build --frontend dockerfile.v0 \
+            --local dockerfile=. \
+            --local context=. \
+            --exporter image \
+            --exporter-opt name=eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}-amd64 \
+            --exporter-opt push=true \
+            --frontend-opt platform=linux/amd64 \
+            --frontend-opt filename=./Dockerfile \
+            --opt build-arg:BUILD_PRODUCTION=true \
+            --opt build-arg:API_URL=http://192.168.2.150
+
+
+# arm
+buildctl build --frontend dockerfile.v0 \
+            --local dockerfile=. \
+            --local context=. \
+            --exporter image \
+            --exporter-opt name=eu.gcr.io/rekhyt-damage-report/dashboard:${TRAVIS_TAG}-armv7 \
+            --exporter-opt push=true \
+            --frontend-opt platform=linux/armhf \
+            --frontend-opt filename=./Dockerfile \
+            --opt build-arg:IMAGE=balenalib/rpi-alpine-node:latest \
+            --opt build-arg:BUILD_PRODUCTION=true \
+            --opt build-arg:API_URL=http://192.168.2.150
+
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+# manifest
+docker manifest create rekhyt-damage-report/dashboard:${TRAVIS_TAG} \
+            rekhyt-damage-report/dashboard:${TRAVIS_TAG}-amd64 \
+            rekhyt-damage-report/dashboard:${TRAVIS_TAG}-armv7
+
+docker manifest annotate rekhyt-damage-report/dashboard:${TRAVIS_TAG} rekhyt-damage-report/dashboard:${TRAVIS_TAG}-armv7 --arch arm
+docker manifest annotate rekhyt-damage-report/dashboard:${TRAVIS_TAG} rekhyt-damage-report/dashboard:${TRAVIS_TAG}-amd64 --arch amd64
+
+docker manifest push rekhyt-damage-report/dashboard:${TRAVIS_TAG}
